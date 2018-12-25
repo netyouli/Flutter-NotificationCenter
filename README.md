c003801d9591505c48da2ce424df521bb5ea61a2
netyouli <712641411@qq.com>
2018-12-25 17:50:14 +0800

    Flutter-NotificationCenter
    A lightweight, easy-to-use notification component library that
    corresponds to Dart supports post notification listening for
    notifications and notification removal

---
+++ README.md
@@ -0,0 +1,78 @@
+
+# Flutter-NotificationCenter
+
+[![build status](https://img.shields.io/travis/flutterchina/dio/vm.svg?style=flat-square)](https://travis-ci.org/flutterchina/Flutter-NotificationCenter)
+[![Pub](https://img.shields.io/pub/v/dio.svg?style=flat-square)](https://pub.dartlang.org/packages/notification_center)
+[![support](https://img.shields.io/badge/platform-flutter%7Cdart%20vm-ff69b4.svg?style=flat-square)](https://github.com/netyouli/Flutter-NotificationCenter)
+[![License MIT](http://img.shields.io/badge/license-MIT-orange.svg?style=flat)](https://raw.githubusercontent.com/crazycodeboy/Flutter-NotificationCenter/master/LICENSE)
+
+A lightweight, easy-to-use notification component library that corresponds to Dart supports post notification listening for notifications and notification removal
+
+## Content
+
+- [Installation](#installation)
+- [Getting started](#getting-started)
+- [API](#api)
+- [Contribution](#contribution)
+
+## Installation
+
+```yaml
+dependencies:
+  notification_center: x.x.x  #latest version
+```
+
+## Getting started  
+
+Add `notification_center.dart` to your js file.
+
+`import 'package:notification_center/notification_center.dart'`
+
+Add observer notification:
+
+```dart
+ NotificationCenter.addObserver(this, 'NotificationName', (param) {
+    ....
+ });
+
+```
+
+Post notification:
+
+```dart
+NotificationCenter.post('NotificationName', 'param value');
+```
+
+Remove specified observer notification:
+
+```dart
+NotificationCenter.removeObserver(this);
+
+```
+
+Remove all observer notification:
+
+```dart
+NotificationCenter.removeObserver(this, 'NotificationName');
+
+```
+
+## API
+
+
+Method   |  Type     | Optional | Description
+----------------- | -------- | -------- | -----------
+post(string, any)   | function | true | post notification
+addObserver(object, string, function)  |   function  |  true   | observer notification
+removeObserver(object, string)  |   function  |  true   | remove observer notification
+
+
+## Contribution
+
+Issues are welcome. Please add a screenshot of bug and code snippet. Quickest way to solve issue is to reproduce it on one of the examples.
+
+Pull requests are welcome. If you want to change API or making something big better to create issue and discuss it first.
+
+---
+
+**MIT Licensed**
---
+++ lib/src/notification_center.dart
@@ -0,0 +1,92 @@
+
+//  Created by WHC on 18/12/25.
+//  Copyright © 2018年 WHC. All rights reserved.
+//
+//  Github <https://github.com/netyouli/Flutter-NotificationCenter>
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+class NotificationCenter {
+    static NotificationCenter _default = new NotificationCenter();
+    var _observerMap = new Map();
+    final _segmentKey = '-888-';
+    
+    /**
+     * 发送通知
+     * name 通知名称
+     * param 通知参数
+     */
+    static void post(String name, [Object param]) {
+        if (name != null) {
+            NotificationCenter._default._observerMap.forEach((key, value){
+                final array = key.split(NotificationCenter._default._segmentKey);
+                if (array.length == 2) {
+                    final _name = array[0];
+                    if (_name == name && value != null) {
+                        value(param);
+                    }
+                }
+            });
+        }else {
+            print('[Notification] post name not null');
+        }
+    }
+
+    /**
+     * 监听通知
+     * observer 监听对象
+     * name 监听的通知名称
+     * block 监听通知的回调
+     */
+    static void addObserver(Object observer, 
+                            String name, 
+                            [void block(Object param)]) {
+        if(observer != null && name != null) {
+            final key = name + NotificationCenter._default._segmentKey + observer.hashCode.toString();
+            NotificationCenter._default._observerMap[key] = block;
+        }else {
+            print('[Notification] observer not null or name not null');
+        }
+    }
+
+
+    /**
+     * 移除通知
+     * observer 要移除的对象
+     * name 要移除的通知名称（如果为空则移除observer下所有通知）
+     */
+    static void removeObserver(Object observer, [String name]) {
+        if (observer != null) {
+            if (name != null) {
+                final key = name + NotificationCenter._default._segmentKey + observer.hashCode.toString();
+                NotificationCenter._default._observerMap.remove(key);
+            }else {
+                final keys = NotificationCenter._default._observerMap.keys;
+                for (var key in keys) {
+                    final array = key.split(NotificationCenter._default._segmentKey);
+                    if (array.length == 2) {
+                        final hasCode = array[1];
+                        if (hasCode == observer.hashCode.toString()) {
+                            NotificationCenter._default._observerMap.remove(key);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
